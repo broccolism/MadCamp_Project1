45c5367c8a2baa2af6d0ea6ea21852cf4d0a63ac
busankimchi <jacky185@kaist.ac.kr>
2019-12-28 13:00:39 +0900

    added address tab

--- app/src/main/java/com/example/senthil/kotlin_tablayout/Fragment/AddressFragment.kt
+++ app/src/main/java/com/example/senthil/kotlin_tablayout/Fragment/AddressFragment.kt
@@ -1,18 +1,97 @@
 package com.example.senthil.kotlin_tablayout.Fragment
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.senthil.kotlin_tablayout.Address
 
 import com.example.senthil.kotlin_tablayout.R
+import com.example.senthil.kotlin_tablayout.RecyclerViewAdapter
+import kotlinx.android.synthetic.main.fragment_address.*
 
 class AddressFragment : Fragment() {
+
+    private var addressList = mutableListOf<Address>()
+    //private lateinit var adapter: RecyclerViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        loadAddress()
+        //setHasOptionsMenu(true)
+    }
+
+    private fun loadAddress() {
+        /*
+        addressList.add(Address("Police", "112"))
+        addressList.add(Address("Ahn Jaewoong", "010-7392-2367"))
+        addressList.add(Address("asdf", "010-1234-5678"))
+         */
+
+        val uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+
+        val selectionArgs = null
+        val sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + "COLLATE LOCALIZED ASC"
+
+        val contactCursor = context?.contentResolver!!.query(uri, projection, null, selectionArgs, sortOrder)
+
+        if (contactCursor!!.moveToFirst()) {
+            do {
+                val phoneNum = contactCursor.getString(1).replace("-","")
+                var number = ""
+
+                if (phoneNum.length == 10) {
+                    number = phoneNum.substring(0, 3) + "-" +
+                            phoneNum.substring(3, 6) + "-" +
+                            phoneNum.substring(6)
+                }
+                else if (phoneNum.length > 8) {
+                    number = phoneNum.substring(0, 3) + "-" +
+                            phoneNum.substring(3, 7) + "-" +
+                            phoneNum.substring(7)
+                }
+
+                val id = contactCursor.getLong(0)
+                val name = contactCursor.getString(2)
+
+                val address = Address(id, number, name)
+                addressList.add(address)
+
+            } while (contactCursor.moveToNext())
+        }
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mRecyclerView?.layoutManager = LinearLayoutManager(this.context)
+        mRecyclerView?.adapter = RecyclerViewAdapter(addressList)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_address, container, false)
+        val view = inflater.inflate(R.layout.fragment_address, container, false)
+
+        return view
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        /*
+        mRecyclerView?.apply {
+            this.layoutManager = LinearLayoutManager(context)
+            this.adapter = adapter
+        }
+         */
     }
 }
