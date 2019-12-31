887e04bee664c4179946f7b7118b55cbfc7ddee5
busankimchi <jacky185@kaist.ac.kr>
2019-12-31 15:58:29 +0900

    dialogList to Activity

--- app/src/main/java/com/example/senthil/kotlin_tablayout/ModifyActivity.kt
+++ app/src/main/java/com/example/senthil/kotlin_tablayout/ModifyActivity.kt
@@ -4,17 +4,16 @@ import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-import kotlinx.android.synthetic.main.testing.*
+import kotlinx.android.synthetic.main.activity_modify.*
 
-class TestingActivity : AppCompatActivity()
+class ModifyActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.testing)
-        var text = findViewById<TextView>(R.id.testing_dialog)
-        text.text = "HI"
+        setContentView(R.layout.activity_modify)
 
+        var consumption = intent.getParcelableExtra<Consumption>("consumption")
+
+        modify.text = consumption.usage
     }
-
-
 }
